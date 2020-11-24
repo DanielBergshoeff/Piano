2bb47a0fef796fc52082ccec8a10e10bc36a6a99
Arye <dabergshoeff@gmail.com>
2020-11-24 21:19:46 +0100

    Added Unity Atoms

--- Assets/Scripts/Variables/IntVariable.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu]
-public class IntVariable : ScriptableObject
-{
-    public int Value;
-}
--- Assets/Scripts/Variables/Vector3Variable.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu]
-public class Vector3Variable : ScriptableObject
-{
-    public Vector3 Value;
-}
