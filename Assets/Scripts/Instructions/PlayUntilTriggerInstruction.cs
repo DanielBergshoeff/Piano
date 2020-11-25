0a5ce0b98b837ce6ff345cc511d8d3952c542b09
Arye <dabergshoeff@gmail.com>
2020-11-25 15:42:09 +0100

    Fixed play till trigger double script

--- Assets/Scripts/Instructions/PlayUntilTriggerInstruction.cs
+++ Assets/Scripts/Instructions/PlayUntilTriggerInstruction.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
 using UnityEngine;
 
-public class PlayUntilTrigger : MonoBehaviour
+[CreateAssetMenu]
+public class PlayUntilTriggerInstruction : Instruction
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public AudioClip SoundToPlay;
+    public StringVariable MyTrigger;
+    public StringEvent MyStringEvent;
+
+    private Piano myPiano;
+    private bool triggerHit = false;
+    private AudioSource myAudioSource;
+
+    public override bool CheckForCompletion() {
+        return triggerHit;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public override void OnInitialize(Piano piano) {
+        myPiano = piano;
+    }
+
+    public override void OnStart() {
+        triggerHit = false;
+        MyStringEvent.Register(TriggerHit);
+        myAudioSource = myPiano.gameObject.AddComponent<AudioSource>();
+        myAudioSource.loop = true;
+        myAudioSource.clip = SoundToPlay;
+        myAudioSource.spatialBlend = 1f;
+        myAudioSource.Play();
+    }
+
+    public override void OnStop() {
+        MyStringEvent.Unregister(TriggerHit);
+        myAudioSource.Stop();
+    }
+
+    public override void OnUpdate() {
+
+    }
+
+    public void TriggerHit(string trigger) {
+        if(trigger == MyTrigger.Value) {
+            triggerHit = true;
+        }
     }
 }
