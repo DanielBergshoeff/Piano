7d1fe36930c8aede32ef8cc649a03ba97ff78cc3
Shade Desktop <gamingkendoka@gmail.com>
2020-11-25 13:31:09 +0100

    Camera controller start

---
+++ Assets/Scripts/FPCameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Rotate self using mouse position
+
+public class FPCameraController : MonoBehaviour
+{
+    public float mouseSensitivity;
+    public GameObject playerBody;
+    float cameraXRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.mousePosition);
+    }
+}
