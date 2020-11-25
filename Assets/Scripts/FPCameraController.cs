c372f3df6fa96e247383246d059e1380b5c56c79
Shade Desktop <gamingkendoka@gmail.com>
2020-11-25 15:30:58 +0100

    Can look around with camera

--- Assets/Scripts/FPCameraController.cs
+++ Assets/Scripts/FPCameraController.cs
@@ -1,24 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
 using UnityEngine;
 
-//Rotate self using mouse position
+//Rotate self and cam using mouse position
+//Mostly got the code from Brackeys
 
 public class FPCameraController : MonoBehaviour
 {
-    public float mouseSensitivity;
+    public FloatVariable mouseSensitivity;
     public GameObject playerBody;
-    float cameraXRotation;
+    public FloatVariable cameraXRotation;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //hide and lock cursor to center of the screen
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.mousePosition);
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity.InitialValue * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity.InitialValue * Time.deltaTime;
+
+        //prevent over-rotate
+        cameraXRotation.InitialValue -= mouseY;
+        cameraXRotation.InitialValue = Mathf.Clamp(cameraXRotation.InitialValue, -90f, 90f);
+
+        //set angles and rotation
+        transform.localRotation = Quaternion.Euler(cameraXRotation.InitialValue, 0f, 0f);
+        playerBody.transform.Rotate(Vector3.up*mouseX);
     }
 }
